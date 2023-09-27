658d08f2c0ae0f72b8ba02c1d18e290e2380b7f1
aymeric-poireau <115146113+aymeric-poireau@users.noreply.github.com>
2023-09-27 15:58:35 +0200

    interaction php-sql

--- Database.php
+++ Database.php
@@ -3,8 +3,11 @@ class Database{
     public $pdo;
 
     public function __construct() {
-        $conDatabase = new PDO("mysql:host=localhost;dbname=moduleconnexionb2", 'root', '');
+        $conDatabase = new PDO("mysql:host=localhost;dbname=aymeric-poireau_mysql", 'aymeric-poireau', 'azertyqwerty345');
         $this->pdo = $conDatabase;
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
     }  
     public function getPdo(){
         return $this->pdo;
